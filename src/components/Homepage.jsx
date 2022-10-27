5100b12c1bc789574c7f8a54ff10c7f84ad3ecfe
Jodylicious <jb418729@gmail.com>
2022-10-27 19:06:40 +0800

    change again

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,7 +3,6 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
 import { Link } from 'react-router-dom';
 // import bot3 from '../assets/images/bot3.png';
 
@@ -15,35 +14,30 @@ const Home = () => {
             {/* create react card */}
 
             <Card className='cardHome'>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="350"
-                        image="https://appinventiv.com/wp-content/uploads/sites/1/2021/08/chatbot-development-with-deep-nlp.png"
-                        alt="chatbot"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5">
-                            HELLO, ka-ISKO at ISKA!
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            "We're no longer teaching people how to communicate with systems, we're teaching systems to communicate with people."
-                        </Typography>
-                        <br />
-                    </CardContent>
+                <CardMedia
+                    component="img"
+                    height="300"
+                    image="https://appinventiv.com/wp-content/uploads/sites/1/2021/08/chatbot-development-with-deep-nlp.png"
+                    alt="chatbot"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5">
+                        HELLO, ka-ISKO at ISKA!
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        "We're no longer teaching people how to communicate with systems, we're teaching systems to communicate with people."
+                    </Typography>
+                </CardContent>
 
-                </CardActionArea>  <div className='startButton'>
                     <button className="startChat">
                         <Link to="/chatapp">
                             <h3 className='chatTitle'>START CONVERSATION</h3>
                         </Link>
                     </button>
-                </div>
-
+                    <h5 className='footer'> © 2022 PUPBC ASKOLAR</h5>
+            
             </Card>
-            <div className='footer'>
-                <h5> © 2022 PUPBC ASKOLAR</h5>
-            </div>
+
         </div>
     );
 };
